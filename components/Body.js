dc3f77ee1cfe637b15cb592e859df1094a4785de
Noxrem <skular.trix@hotmail.ch>
2022-04-18 00:33:04 +0200

    Initial poem does work now, but a refresh doesn't

--- components/Body.js
+++ components/Body.js
@@ -2,32 +2,38 @@ import { useState } from 'react';   // To update the poems
 import data from '../static/schnupf_dataset.json';
 import Poetry_Textbox from "../components/Poetry_Textbox";
 import Poetry_Buttons from "../components/Poetry_Buttons";
+import { getPoem } from '../components/getPoem';
 
-const Body = () => {
-    const nofPoems = data.length;       // Number of poems in the database
-
-    // Calculate random index of poem (between 1 and number of poems)
-    const getRandomPoemIdx = () => {                      
-        return Math.floor(Math.random() * nofPoems-1) + 1;   
-    }
+const Body = (props) => {
     
-    const getPoem = (poemIdx) => {
-        return data[poemIdx].text;
-    }
+    //const nofPoems = data.length;       // Number of poems in the database
+    // Calculate random index of poem (between 1 and number of poems)
+    // const getRandomPoemIdx = () => {                      
+    //     return Math.floor(Math.random() * nofPoems-1) + 1;   
+    // }
+    
+    // const getPoem = (poemIdx) => {
+    //     return data[poemIdx].text;
+    // }
 
-    const [poemIdx, setPoemIdx] = useState(6);    // Initial poem in the textbox
+    // const [poemIdx, setPoemIdx] = useState(6);    // Initial poem in the textbox
 
 
     const updatePoem = () => {
-        setPoemIdx(getRandomPoemIdx(nofPoems));  // Set the next poem randommly
+        //setPoemIdx(getRandomPoemIdx(nofPoems));  // Set the next poem randommly
+        props.poem = getPoem();
+        this.setState({ poem: getPoem() });
     }
+
+    
 
     return (
         <div className="body-wrapper">
-            <Poetry_Textbox text={getPoem(poemIdx)}/>
+            <Poetry_Textbox text={props.poem}/*text={getPoem(poemIdx)}*//>
             <Poetry_Buttons clickRefresh={updatePoem}/>
         </div>
     )
 
 }
+
 export default Body;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Layout from '../components/Layout';
 import Body from '../components/Body';
+import data from '../static/schnupf_dataset.json';
+import { getPoem } from '../components/getPoem';
 
-const Homepage = () => (
+const Homepage = ({ initPoem }) => (
     <Layout>
         <h1>Welcome to the Snuff Poetry website!</h1>
-        <Body />
+        <Body poem={initPoem}/>
     </Layout>
   );
   
-  export default Homepage;
+// Populate the website with the initial poem
+export async function getServerSideProps() {
+    const initPoem = getPoem(); // Get an initial poem
+    return { props: { initPoem }}
+}
+export default Homepage;
   
