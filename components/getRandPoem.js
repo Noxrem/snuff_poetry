64002df2f1c50c8d0aa885886ae0abcbb5c3afd6
Noxrem <skular.trix@hotmail.ch>
2022-04-19 00:14:09 +0200

    change to julou95 solution
    the json file can not be fetched. The object seems to be undefined.

--- components/getRandPoem.js
+++ components/getRandPoem.js
@@ -1,8 +1,6 @@
-import data from '../static/schnupf_dataset.json';
-
 // Get a random poem from the dataset
-export function getPoem() {
+export function getRandPoem(data) {
     const nofPoems = data.length;
     let randIdx = Math.floor(Math.random() * nofPoems-1) + 1;
-    return data[randIdx].text;
+    return data[randIdx];
 } 
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
 import Layout from '../components/Layout';
 import Body from '../components/Body';
-import data from '../static/schnupf_dataset.json';
-import { getPoem } from '../components/getPoem';
+//import data from '../static/schnupf_dataset.json';
+import { getRandPoem } from '../components/getRandPoem';
 
-const Homepage = ({ initPoem }) => (
+const Homepage = ({ poems, initPoem }) => (
     <Layout>
         <h1>Welcome to the Snuff Poetry website!</h1>
-        <Body poem={initPoem}/>
+        <Body poems={poems} poem={initPoem}/>
     </Layout>
   );
   
 // Populate the website with the initial poem
-export async function getServerSideProps() {
-    const initPoem = getPoem(); // Get an initial poem
-    return { props: { initPoem }}
+/*export async function getServerSideProps(context) */
+Homepage.getInitialProps = async (context) => {
+    const data = await import('../static/schnupf_dataset.json');
+    console.log('get new', data.text);
+    const initPoem = getRandPoem(data);     // Get an initial poem
+
+    return { poems: data, initPoem: initPoem } // Return dataset of poems and the initial poem
 }
 export default Homepage;
   
