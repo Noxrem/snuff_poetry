6b4dab84991469fe50398b93244e31f579c438c2
Noxrem <skular.trix@hotmail.ch>
2022-04-28 23:53:14 +0200

    Added new line in the poetry_textbox

--- components/Poetry_Textbox.js
+++ components/Poetry_Textbox.js
@@ -1,7 +1,7 @@
 const Poetry_Textbox = ({ poem }) => (
     <div className="poetry-textbox">
         <h2>{poem.title}</h2>
-        <p>{poem.text}</p>
+        <p style={{ whiteSpace: 'pre' }}>{poem.text}</p>
     </div>
 );
 
