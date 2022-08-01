c9003bbde50904afc01463978703d6da9d5f90c4
Noxrem <skular.trix@hotmail.ch>
2022-08-01 09:56:25 +0800

    Add line breaks to the Poetry_Textbox

--- src/components/Poetry_Textbox.js
+++ src/components/Poetry_Textbox.js
@@ -1,7 +1,7 @@
 const Poetry_Textbox = ({ poem }) => (
     <div className="poetry-textbox">
         <h2 className="text-3xl underline">{poem.title}</h2>
-        <p>{poem.text}</p>
+        <p className="whitespace-pre-wrap">{poem.text}</p>
     </div>
 );
 
