2a2b528982f73fd527bb1b698e79a35b89c0fb0d
Noxrem <skular.trix@hotmail.ch>
2023-08-04 13:48:15 +0200

    Update next.config.js
    Disabled automatic locale detection.

--- next.config.js
+++ next.config.js
@@ -4,5 +4,7 @@ module.exports = {
         locales: ["all", "ch", "de", "en"],
         // default locale used when the non-locale path s are visited
         defaultLocale: "all",
+        // Disable the automatic redirection based on the user's preferred locale
+        localeDetection: false,
     },
 };
