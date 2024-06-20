96ee8eabb02e38cad5a54b29e9c2d4b29416b285
Noxrem <skular.trix@hotmail.ch>
2024-06-20 22:06:28 +0200

    Add vercel analytics
    - add analytics
    - npm audit fix vulnerabilities

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,12 +1,13 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Analytics } from "@vercel/analytics/react";  // Adding Vercel analytics
 
 const Layout = (props) => (
   <div className="site-wrapper h-screen max-h-full flex flex-col">
     <Header {...props}/>
 
     {props.children}
-
+    <Analytics/>
     <Footer />
   </div>
 );
