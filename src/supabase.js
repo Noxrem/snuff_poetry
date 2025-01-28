7e944e195f3aec22301d71922fdffaece8d0378f
Noxrem <skular.trix@hotmail.ch>
2025-01-28 21:04:21 +0100

    Moved environmental values into .env.local

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pcnatpkkokguqrpwpnlo.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjbmF0cGtrb2tndXFycHdwbmxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5MjYxNTksImV4cCI6MjA1MzUwMjE1OX0.S-buPLXG-MHpY-9SS1cNvgNIg0AgKO6bX9ZU3oHVezM";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
