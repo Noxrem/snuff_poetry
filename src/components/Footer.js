16039a3b75e459f4a5a0c88ec1c35901383a0d99
Noxrem <skular.trix@hotmail.ch>
2025-04-06 20:53:04 +0200

    Update Footer.js
    Added same margin for Footer as for the other containers.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ const Footer = () => (
         <div className="
             flex justify-center
             rounded
-            bg-white">© {new Date().getFullYear()} Gentlemen's Snuff</div>
+            bg-white
+            mx-1
+            my-1">© {new Date().getFullYear()} Gentlemen's Snuff</div>
     </>
 );
 
